8336dbded9d8a80f01ee9b5ef8bfae5a0554e083
FHSU-dude <188289469+QuantumOfThought@users.noreply.github.com>
2025-09-15 19:55:00 -0500

    Add main.py for Stock Tickers mini project with initial implementation

---
+++ main.py
@@ -0,0 +1,47 @@
+### INF601 - Advanced Programming in Python
+### Jeremy McKowski
+### Mini Project 1
+### Stock Tickers
+
+import numpy as np
+import matplotlib.pyplot as plt
+import yfinance as yf
+import os
+
+# Project Title
+
+Miniproject 1
+Stock Tickers
+
+## Description
+
+This program will lookup 5 stock tickers and will present the user with 5 graphs.
+Those graphs are automatically saved in the charts / folder.
+
+## Getting Started
+
+### Dependencies
+
+Please install the pip requirements: ``` pip install - r requirements.txt```
+
+### Executing program
+
+```
+python
+main.py
+```
+
+## Authors
+
+Jeremy McKowski
+
+## Version History
+
+*0.1
+*Initial Release
+
+## Acknowledgments
+
+*[Matplotlib Documentation](https: // matplotlib.org / stable / users / index.html)
+*[yFinance Documentation](https: // ranaroussi.github.io / yfinance /)
+* [Jason Zeller](https://www.youtube.com/@profzeller)
