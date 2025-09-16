582ff388d4792cc1b8ddd8953d45d1e69bd07582
FHSU-dude <188289469+QuantumOfThought@users.noreply.github.com>
2025-09-15 20:48:02 -0500

    Add missing newline in main.py for consistency

--- main.py
+++ main.py
@@ -15,6 +15,7 @@
 # (10/10 points) I will be checking out the main branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this file with the output of pip freeze at the terminal prompt.
 # (20/20 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown.
 
+
 import yfinance as yf
 import pprint
 import numpy as np
